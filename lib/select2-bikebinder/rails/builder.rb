9b95d62fb95091a11cba90ff6de472a6bb15ea85
William Wedler <wwedler@riseup.net>
2013-01-09 01:08:33 -0500

    model and functions for selecting brand

--- lib/select2-bikebinder/rails/builder.rb
+++ lib/select2-bikebinder/rails/builder.rb
@@ -10,6 +10,11 @@ module Select2BikeBinder
       def default_options
         super.merge({:width=>'400px'})
       end
+
+      def to_partial_path
+        "#{partial_path_root}/hidden_input_select"
+      end
+
     end
 
     class ColorSelect < SelectBuilder
@@ -26,7 +31,7 @@ module Select2BikeBinder
           [ColorNameI18n::Color.new(k).name.capitalize, k]
         end
       end
-    end
+    end # class ColorSelect
 
     class ModelNestedBrandSelect < SelectBuilder
       def self.selector_class; "bike_model_nested_brand_sel" end
@@ -42,7 +47,33 @@ module Select2BikeBinder
         end
         @css
       end
-    end
 
-  end
-end
+      def to_partial_path
+        "#{partial_path_root}/hidden_input_select"
+      end
+
+    end # class ModelNestedBrandSelect
+
+    class BrandSelect < SelectBuilder
+      def self.selector_class; "bike_brand_sel" end      
+
+      def default_options
+        super.merge({:width=>'600px'})
+      end
+
+      def css_class
+        unless @css
+          @css = super
+          @css += " bigdrop"
+        end
+        @css
+      end
+
+      def to_partial_path
+        "#{partial_path_root}/hidden_input_select"
+      end
+
+    end # class Brand Select
+
+  end # module Builder
+end # module Select2BikeBinder
